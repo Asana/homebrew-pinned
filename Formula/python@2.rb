5f33735e1f393c23568006a7910825e10affe4ee
Theo Spears <theo@asana.com>
2020-02-19 11:41:21 -0800

    Use our own hosted versions of python@2 bottles
    We don't know how long the ones hosted by brew will continue to exist now
    the formula has been deleted, so mirror the binaries too

--- Formula/python@2.rb
+++ Formula/python@2.rb
@@ -7,6 +7,7 @@ class PythonAT2 < Formula
   head "https://github.com/python/cpython.git", :branch => "2.7"
 
   bottle do
+    root_url "https://asana-oss-cache.s3.amazonaws.com/bottles"
     sha256 "accfaa922708f00afb69ab230199f96e6ecdddd248a1eca586ce1e5e5cfd732b" => :catalina
     sha256 "54d3351d6be8268b2f5017894dcc8e083811dfa3812bdb9f79f989873b9a4542" => :mojave
     sha256 "cfd5c6eeac37065d19f527bb0798a9caf1928bab3340cd545224861a3c82f219" => :high_sierra
